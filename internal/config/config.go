cb9c01dba52b19b4e2ff459f8007659e6677cbe8
korolev.andrey <korolev.andrey15@wildberries.work>
2026-09-20 18:47:05 +0500

    Increase default agent steps to 48

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,7 +19,7 @@ type Config struct {
 func Load() Config {
 	return Config{
 		Token: os.Getenv("MCP_LLM_TOKEN"), BaseURL: strings.TrimRight(os.Getenv("MCP_LLM_BASE_URL"), "/"), Model: os.Getenv("MCP_LLM_MODEL"),
-		MaxSteps: envInt("MCP_MAX_AGENT_STEPS", 24, 1, 50), MaxInputChars: envInt("MCP_MAX_INPUT_CHARS", 12000, 256, 100000),
+		MaxSteps: envInt("MCP_MAX_AGENT_STEPS", 48, 1, 50), MaxInputChars: envInt("MCP_MAX_INPUT_CHARS", 12000, 256, 100000),
 		MaxFileChars: envInt("MCP_MAX_FILE_CHARS", 24000, 1024, 200000), MaxTotalContextChars: envInt("MCP_MAX_TOTAL_CONTEXT_CHARS", 160000, 4096, 1000000),
 		MaxOutputTokens: envInt("MCP_MAX_OUTPUT_TOKENS", 3000, 128, 16000), RequestTimeout: time.Duration(envInt("MCP_REQUEST_TIMEOUT_MS", 60000, 1000, 600000)) * time.Millisecond, CommandTimeout: time.Duration(envInt("MCP_COMMAND_TIMEOUT_MS", 60000, 1000, 600000)) * time.Millisecond,
 	}
